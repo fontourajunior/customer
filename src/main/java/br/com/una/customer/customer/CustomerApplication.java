3e3a9c06a2d5fb2fe7bf016e69b9daa116cc07f3
Maxwell Dantas <maxwelldsouza@hotmail.com>
2018-11-06 19:39:53 -0200

    corsConfigurer

--- src/main/java/br/com/una/customer/customer/CustomerApplication.java
+++ src/main/java/br/com/una/customer/customer/CustomerApplication.java
@@ -2,6 +2,10 @@ package br.com.una.customer.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class CustomerApplication {
@@ -9,4 +13,14 @@ public class CustomerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerApplication.class, args);
 	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
+	};
 }
